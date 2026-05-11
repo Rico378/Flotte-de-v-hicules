960133b3ca168018b67f3d8bf10eb11111bde574
Rico378 <autrenom75@gmail.com>
2026-05-11 14:45:41 +0200

    ajout du vehicule

---
+++ src/model/Vehicule.java
@@ -0,0 +1,24 @@
+package model;
+
+// Classe abstraite centrale [cite: 51]
+public abstract class Vehicule implements Maintenable, Assignable {
+    protected String immatriculation;
+    protected String marque;
+    protected int kilometrage;
+    protected boolean disponible;
+
+    public Vehicule(String immatriculation, String marque, int kilometrage) {
+        this.immatriculation = immatriculation;
+        this.marque = marque;
+        this.kilometrage = kilometrage;
+        this.disponible = true;
+    }
+
+    // Méthode abstraite obligatoire pour le barème [cite: 108]
+    public abstract double calculerCoutAssurance();
+
+    // Getters
+    public String getImmatriculation() { return immatriculation; }
+    public int getKilometrage() { return kilometrage; }
+    
+}
