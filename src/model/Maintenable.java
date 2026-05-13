0d82b26dad4a912bc29d26d6919ca8049bfeee4d
Rico378 <autrenom75@gmail.com>
2026-05-13 09:37:35 +0200

    maintenance

--- src/model/Maintenable.java
+++ src/model/Maintenable.java
@@ -1,30 +1,16 @@
 package model;
 
-/**
- * Interface Maintenable : Contrats pour les véhicules nécessitant une maintenance.
- * Concept OOP : Ségrégation des interfaces (ISP - Interface Segregation Principle)
- */
+
 public interface Maintenable {
-    /**
-     * Marquer le véhicule comme nécessitant une maintenance
-     * @param description Raison de la maintenance
-     */
+   
     void signalerEntretien(String description);
     
-    /**
-     * Effectuer la maintenance du véhicule
-     */
+ // Effectuer la maintenance et remettre le véhicule en service
     void effectuerEntretien();
     
-    /**
-     * Vérifier si le véhicule nécessite une maintenance
-     * @return true si maintenance requise, false sinon
-     */
+   
     boolean necessiteEntretien();
     
-    /**
-     * Obtenir le coût estimé de la maintenance
-     * @return Coût en euros
-     */
+   
     double obtenirCoutEntretien();
 }
